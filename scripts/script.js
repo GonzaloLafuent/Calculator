b937665ca277608519bcfc0b59f69f710a96752b
Gonzalo Matias  Lafuente <lafuentegonzalo1@gmail.com>
2024-01-15 23:54:47 -0300

    funcionando operaciones con teclado

--- scripts/script.js
+++ scripts/script.js
@@ -8,42 +8,54 @@ const btn_borrar_todo = document.querySelector(".btn-borrar-todo");
 const btn_borrar = document.querySelector(".btn-borrar")
 const botones = document.querySelectorAll("button");
 
-let op1 = ["",0];
-let op2 = ["",0];
+const op1 = {
+    value: "",
+    ready: 0
+}
+
+const op2 = {
+    value: "",
+    ready: 0
+}
+
+/* no olviadar de modiciar que op2[0] 0 value y op2[1]= available, palica para los dos op */
 let symbol = "";
-let nuevo_simbolo = 0;
-let result = 0;
+let result = "";
 
 function writeExpresion(exp){
     expresion.textContent +=exp;
 }
 
-function updateOp(){
-    
+function updateOp(e){
+    if(e==="+"||e==="-"||e==="/" || e==="." || e === "*" || e === "Enter"){
+        if(!op1.ready) op1.ready = 1;
+        else if(!op2.ready) op2.ready = 1;
+    }
 }
 
 function load_op(op){
     if(op>=0 || op<=9){
-        if(op1[1]==0){
-            op1[0] += op;
-            op1[1] = nuevo_simbolo==0?0:1;
-        } else if(op2[1]==0){
-            op2[0] = op;
-            op2[1] = symbol===""? 0:1;
-        } else{
-            operate();
-            op1[0] = result;
-            op2[0] = op;
+        if(!op1.ready){
+            op1.value += op;
+        } else if(!op2.ready){
+            op2.value += op;
         }
-    } else { 
+    } else {
+        if(op1.ready && op2.ready){
+            operate();
+            op1.value = ""+result;
+            op2.ready = 0;
+            op2.value = "";
+        }    
         symbol = op;
     }    
 }
 
+
 function operate(){
-    let op1_numb = parseInt(op1[0]);
-    let op2_numb = parseInt(op2[0]);
-    if(op1[1]==1 && op2[1]==1  && symbol!=""){
+    if(op1.ready && op2.ready){
+        let op1_numb = (op1.value.includes("."))?parseFloat(op1.value):parseInt(op1.value);
+        let op2_numb = (op2.value.includes("."))?parseFloat(op2.value):parseInt(op2.value);
         if(symbol=="+"){
             result = op1_numb + op2_numb; 
         } else if(symbol=="-"){
@@ -53,7 +65,6 @@ function operate(){
         } else if(symbol=="/"){
             result = op1_numb/ op2_numb;
         }
-        symbol = "";
     }
 }
 
@@ -65,15 +76,21 @@ window.addEventListener("keydown",(e)=>{
         writeExpresion(e.key);
     } else if(e.key==="+"||e.key==="-"||e.key==="/" || e.key==="." || e.key === "*"){
         updateOp(e.key);
-        load_op(e.key);
+        load_op(e.key==="*"?"x":e.key);
         operate();
         writeExpresion(e.key==="*"?"x":e.key);    
     } else if(e.key==="Backspace"){
         expresion.textContent = "";
+        result = "";
+        op1.value = ""; op1.ready = 0;
+        op2.value = ""; op2.ready = 0;
+        resultado.textContent = "";
     } else if(e.key==="Enter") {
+        updateOp(e.key)
+        operate();
         resultado.textContent = result;
-        op1[0] = 0,op1[1] = 0;
-        op2[0] = 0,op2[1] = 0;
+        op1.value = ""+result; op1.ready = 0;
+        op2.value = "",op2.ready = 0;
     }    
 });
 
