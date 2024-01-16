dbf32765df4d02f62d160b11ac6184dcda90a1e1
Gonzalo Matias  Lafuente <lafuentegonzalo1@gmail.com>
2024-01-16 00:19:21 -0300

    agrego funciones de botones

--- scripts/script.js
+++ scripts/script.js
@@ -8,100 +8,114 @@ const btn_borrar_todo = document.querySelector(".btn-borrar-todo");
 const btn_borrar = document.querySelector(".btn-borrar")
 const botones = document.querySelectorAll("button");
 
-const op1 = {
+const first_operand = {
     value: "",
     ready: 0
 }
 
-const op2 = {
+const second_operand = {
     value: "",
     ready: 0
 }
 
 /* no olviadar de modiciar que op2[0] 0 value y op2[1]= available, palica para los dos op */
-let symbol = "";
+let operator = "";
 let result = "";
 
 function writeExpresion(exp){
     expresion.textContent +=exp;
 }
 
-function updateOp(e){
-    if(e==="+"||e==="-"||e==="/" || e==="." || e === "*" || e === "Enter"){
-        if(!op1.ready) op1.ready = 1;
-        else if(!op2.ready) op2.ready = 1;
+function setOperandReady(operator){
+    if(operator==="+"||operator==="-"||operator==="/" || operator === "*" || operator === "Enter"){
+        if(!first_operand.ready) first_operand.ready = 1;
+        else if(!second_operand.ready) second_operand.ready = 1;
     }
 }
 
-function load_op(op){
-    if(op>=0 || op<=9){
-        if(!op1.ready){
-            op1.value += op;
-        } else if(!op2.ready){
-            op2.value += op;
+function parseSymbols(symbol){
+    if((symbol>=0 || symbol<=9) || symbol==="."){
+        if(!first_operand.ready){
+            first_operand.value += symbol;
+        } else if(!second_operand.ready){
+            second_operand.value += symbol;
         }
     } else {
-        if(op1.ready && op2.ready){
+        if(first_operand.ready && second_operand.ready){
             operate();
-            op1.value = ""+result;
-            op2.ready = 0;
-            op2.value = "";
+            first_operand.value = ""+result;
+            second_operand.ready = 0;
+            second_operand.value = "";
         }    
-        symbol = op;
+        operator = symbol;
     }    
 }
 
 
 function operate(){
-    if(op1.ready && op2.ready){
-        let op1_numb = (op1.value.includes("."))?parseFloat(op1.value):parseInt(op1.value);
-        let op2_numb = (op2.value.includes("."))?parseFloat(op2.value):parseInt(op2.value);
-        if(symbol=="+"){
-            result = op1_numb + op2_numb; 
-        } else if(symbol=="-"){
-            result =  op1_numb - op2_numb;
-        } else if(symbol=="x"){
-            result = op1_numb * op2_numb;
-        } else if(symbol=="/"){
-            result = op1_numb/ op2_numb;
+    if(first_operand.ready && second_operand.ready){
+        let first_operand_num = (first_operand.value.includes("."))?parseFloat(first_operand.value):parseInt(first_operand.value);
+        let second_operand_num = (second_operand.value.includes("."))?parseFloat(second_operand.value):parseInt(second_operand.value);
+        if(operator==="+"){
+            result = first_operand_num + second_operand_num; 
+        } else if(operator==="-"){
+            result =  first_operand_num - second_operand_num;
+        } else if(operator==="x"){
+            result = first_operand_num * second_operand_num;
+        } else if(operator==="/"){
+            result = first_operand_num / second_operand_num;
         }
     }
 }
 
 window.addEventListener("keydown",(e)=>{
     if(e.key>=0 && e.key<=9){ 
-        updateOp(e.key);
-        load_op(e.key);
-        operate();
+        setOperandReady(e.key);
+        parseSymbols(e.key);
         writeExpresion(e.key);
     } else if(e.key==="+"||e.key==="-"||e.key==="/" || e.key==="." || e.key === "*"){
-        updateOp(e.key);
-        load_op(e.key==="*"?"x":e.key);
-        operate();
+        setOperandReady(e.key);
+        parseSymbols(e.key==="*"?"x":e.key);
         writeExpresion(e.key==="*"?"x":e.key);    
     } else if(e.key==="Backspace"){
         expresion.textContent = "";
         result = "";
-        op1.value = ""; op1.ready = 0;
-        op2.value = ""; op2.ready = 0;
+        first_operand.value = ""; first_operand.ready = 0;
+        second_operand.value = ""; second_operand.ready = 0;
         resultado.textContent = "";
     } else if(e.key==="Enter") {
-        updateOp(e.key)
+        setOperandReady(e.key)
         operate();
         resultado.textContent = result;
-        op1.value = ""+result; op1.ready = 0;
-        op2.value = "",op2.ready = 0;
+        first_operand.value = ""+result; first_operand.ready = 0;
+        second_operand.value = "",second_operand.ready = 0;
     }    
 });
 
 botones.forEach(boton => {
     boton.addEventListener("click",(e)=>{
-
+        txt_btn = e.target.textContent;
+        if(txt_btn>=0 && txt_btn<=9){ 
+            setOperandReady(txt_btn==="x"?"*":txt_btn);
+            parseSymbols(txt_btn);
+            writeExpresion(txt_btn);
+        } else if(txt_btn==="+"||txt_btn==="-"||txt_btn==="/" || txt_btn==="." || txt_btn === "x"){
+            setOperandReady(txt_btn==="x"?"*":txt_btn);
+            parseSymbols(txt_btn);
+            writeExpresion(txt_btn);    
+        } else if(txt_btn==="BORRAR TODO"){
+            expresion.textContent = "";
+            result = "";
+            first_operand.value = ""; first_operand.ready = 0;
+            second_operand.value = ""; second_operand.ready = 0;
+            resultado.textContent = "";
+        } else if(txt_btn==="=") {
+            setOperandReady("Enter")
+            operate();
+            resultado.textContent = result;
+            first_operand.value = ""+result; first_operand.ready = 0;
+            second_operand.value = "",second_operand.ready = 0;
+        }    
     });
 });
 
-function init(){
-    
-}
-
-init();
